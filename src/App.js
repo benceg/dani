944785330f3e71880b59d85f23208c8c8229d16f
Benjamin Ceglowski <ben@thephuse.com>
2016-08-18 15:50:44 +0200

    Early stateful component pulls data from Prismic.
    Installed some dependencies.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import { api, Predicates } from 'prismic.io';
+
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      posts: []
+    };
+  }
+
+  componentDidMount() {
+    api('https://daniellebooysen-test.prismic.io/api')
+      .then(api => api.query(Predicates.at('document.type', 'albums')))
+      // .then(api => api.query(Predicates.at('my.albums.uid', 'new-story')))
+      .then(response => this.setState({posts: response.results}));
+  }
+
   render() {
+    const {
+      posts
+    } = this.state
+    console.log(posts)
     return (
       <div className="App">
         <div className="App-header">
@@ -13,9 +34,15 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <ul>
+          {posts.map(post =>
+            <li key={post.id}>{post.getText('albums.title')}</li>
+          )}
+        </ul>
       </div>
     );
   }
+
 }
 
 export default App;
