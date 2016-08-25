16d887daa3ea820ad60521f064be38f4e32f2cfa
Ben Ceglowski <bceglowski@gmail.com>
2016-08-25 23:40:28 +0200

    Let's try to export that constant again, shall we?

--- src/helpers/contentful.js
+++ src/helpers/contentful.js
@@ -1,6 +1,8 @@
 import contentful from 'contentful';
 
-export default const client = contentful.createClient({
+const client = contentful.createClient({
   space: 'fya5qoac19z2',
   accessToken: 'fba1869143fff7e256ec15c58b18fb2b0d9efc66c3ed102f8765be4e0b25eea2'
 });
+
+export default const;
