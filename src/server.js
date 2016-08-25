b31014f16d621787b6642c8d03fc384959cacafb
Ben Ceglowski <bceglowski@gmail.com>
2016-08-25 23:59:08 +0200

    Rectified distribution folders.

--- src/server.js
+++ src/server.js
@@ -27,7 +27,7 @@ if(process.env.NODE_ENV === 'development') {
 	app.use(require('webpack-hot-middleware')(compiler));
 	app.use(express.static(path.resolve(__dirname, 'src')));
 } else if(process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.resolve(__dirname, 'dist')));
+	app.use(express.static(path.resolve(__dirname, '../dist')));
 }
 
 app.get('*', middleware);
