8ecb14b3b2d8fb0d78d0071b65be8d4addca53e5
Benjamin Ceglowski <bceglowski@gmail.com>
2016-09-30 12:19:11 +0100

    Allowed flexible image quality.

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -3,13 +3,13 @@ import React from 'react';
 import MediaQuery from 'react-responsive'
 
 const sizes = [
-  { breakpoint: 1681, width: 1920, height: 1280 },
-  { breakpoint: 1441, width: 1680, height: 1120 },
-  { breakpoint: 1081, width: 1440, height: 960 },
-  { breakpoint: 961, width: 1080, height: 720 },
-  { breakpoint: 769, width: 960, height: 640 },
-  { breakpoint: 641, width: 768, height: 512 },
-  { breakpoint: 321, width: 660, height: 440 }
+  { breakpoint: 1681, width: 1920, height: 1280, quality: 80 },
+  { breakpoint: 1441, width: 1680, height: 1120, quality: 80 },
+  { breakpoint: 1081, width: 1440, height: 960, quality: 80 },
+  { breakpoint: 961, width: 1080, height: 720, quality: 80 },
+  { breakpoint: 769, width: 960, height: 640, quality: 80 },
+  { breakpoint: 641, width: 768, height: 512, quality: 90 },
+  { breakpoint: 321, width: 660, height: 440, quality: 90 }
 ];
 
 const Image = ({
@@ -25,10 +25,11 @@ const Image = ({
       sizes.map(({
         breakpoint,
         width,
-        height
+        height,
+        quality
       }, index) =>
         <MediaQuery key={`${src}${width}${height}`} minDeviceWidth={sizes[index + 1] ? breakpoint : 0} maxDeviceWidth={sizes[index - 1] ? sizes[index - 1].breakpoint : null}>
-          <img alt={alt} title={title} className={className} src={`${src}?fit=thumb&w=${width}&h=${height}&q=80`} />
+          <img alt={alt} title={title} className={className} src={`${src}?fit=thumb&w=${width}&h=${height}&q=${quality}`} />
         </MediaQuery>
       )
     :
